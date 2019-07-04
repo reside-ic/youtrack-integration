1a8128b35eeb27b6b9a9c76661c9361cd81c05d5
Alex <alex.hill@gmail.com>
2019-07-04 15:18:33 +0100

    correct pytest command

--- teamcity.sh
+++ teamcity.sh
@@ -4,7 +4,7 @@ git_id=$(git rev-parse --short=7 HEAD)
 git_branch=$(git symbolic-ref --short HEAD)
 
 pip3 install --quiet -r requirements-dev.txt
-pytest
+python3 -m pytest
 
 registry=docker.montagu.dide.ic.ac.uk:5000
 name=youtrack-integration-webhook
