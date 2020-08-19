923316f395d09eeaa04da8c3e0eb60d891c17136
Alex <alex.hill@gmail.com>
2020-08-19 14:04:03 +0100

    run tests in docker

--- buildkite/build.sh
+++ buildkite/build.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
+set -e
+HERE=$(dirname $0)
+. $HERE/common
 
-git_id=$(git rev-parse --short=7 HEAD)
-git_branch=$(git symbolic-ref --short HEAD)
+NAME=youtrack-integration-webhook
+COMMIT_TAG=$ORG/$NAME:$GIT_SHA
+BRANCH_TAG=$ORG/$$NAME:$GIT_BRANCH
 
-pip3 install --quiet -r requirements-dev.txt
-python3 -m pytest
-
-org=vimc
-name=youtrack-integration-webhook
-
-commit_tag=$org/$name:$git_id
-branch_tag=$org/$name:$git_branch
-
-docker build -t $commit_tag -t $branch_tag .
-docker push $commit_tag
-docker push $branch_tag
+docker build -t $COMMIT_TAG -t $BRANCH_TAG .
+docker push $COMMIT_TAG
+docker push $BRANCH_TAG
